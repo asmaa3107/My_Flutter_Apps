35c5d75e824eba5bf51d5942e749d207aaf66911
unknown <asmaa3107@gmail.com>
2020-01-21 18:51:34 +0200

    qizzer modifcation class

---
+++ quizzler_flutter_app/lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+import 'Classes/Questions.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> answerIcons = [];
+  int qNum = 0;
+  List<Question> qList = [
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                qList[qNum].questionText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                bool correctAnswer = qList[qNum].questionAnswer;
+                if (correctAnswer == true) {
+                  answerIcons.add(Icon(Icons.check, color: Colors.green));
+                  print('user got it Right ..');
+                } else {
+                  answerIcons.add(Icon(Icons.close, color: Colors.red));
+                  print('user got it Wrong ..');
+                }
+
+                setState(() {
+                  //print(qNum);
+                  if (qNum <= 2) {
+                    qNum++;
+                  }
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                bool correctAnswer = qList[qNum].questionAnswer;
+                if (correctAnswer == false) {
+                  answerIcons.add(Icon(Icons.check, color: Colors.green));
+                  print('user got it Right ..');
+                } else {
+                  answerIcons.add(Icon(Icons.close, color: Colors.red));
+                  print('user got it Wrong ..');
+                }
+
+                setState(() {
+                  if (qNum <= 2) {
+                    qNum++;
+                  }
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: answerIcons,
+        ),
+      ],
+    );
+  }
+}
+
+/*
+
+*/
